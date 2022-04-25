a46c4c35da344ec01c77b632e57589f6094377b1
reifus1 <reifus1@gmail.com>
2022-04-25 13:03:55 +0200

    finished up user routes

--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,8 @@ router.get('/', async (req, res) => {
 });
 
 // Geting one
-router.get('/:id', (req, res) => {
-    res.send(`User id: ${req.params.id}`);
+router.get('/:id', getUser, (req, res) => {
+    res.json(res.user);
 });
 
 // Creating one
@@ -37,13 +37,50 @@ router.post('/', async (req, res) => {
 });
 
 // Updating one
-router.patch('/:id', (req, res) => {
-
+router.patch('/:id', getUser, async (req, res) => {
+    if (req.body.email != null) {
+        res.body.email = req.body.email;
+    }
+    if (req.body.name != null) {
+        res.user.name = req.body.name;
+    }
+    if (req.body.isActive != null) {
+        res.user.isActive = req.body.isActive;
+    }
+    if (req.body.loginCount != null) {
+        res.user.loginCount = req.body.loginCount;
+    }
+    res.user.updated = Date.now();
+    try {
+        const updatedUser = await res.user.save();
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 });
 
 // Deleting one
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', getUser, async (req, res) => {
+    try {
+        await res.user.remove();
+        res.json({ message: 'User deleted.'});
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
+
+async function getUser(req, res, next) {
+    let user;
+    try {
+        user = await User.findById(req.params.id);
+        if (user == null) {
+            return res.status(404).json({ message: 'User not found.'});
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message});
+    }
+    res.user = user;
+    next();
+}
 
 module.exports = router;
